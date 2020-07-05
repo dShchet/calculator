8a67eee46b9dea01cdebc8b9cd1aa72004a96239
unknown <d.shchet@gmail.com>
2020-07-05 18:31:13 +0300

    Updates

---
+++ precache-manifest.919dd400ced307b2c970059d441cc72f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6f3e28210e2bd292ed1ed0474bff26",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7f212525e4728d0dcce4",
+    "url": "/calculator/static/css/main.5808d54d.chunk.css"
+  },
+  {
+    "revision": "32abe062e97c5c1adae4",
+    "url": "/calculator/static/js/2.54d28910.chunk.js"
+  },
+  {
+    "revision": "7f212525e4728d0dcce4",
+    "url": "/calculator/static/js/main.7400fe7d.chunk.js"
+  },
+  {
+    "revision": "f69b5c5b50f5b7cd07bd",
+    "url": "/calculator/static/js/runtime-main.736c2c55.js"
+  }
+]);
